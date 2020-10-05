b3cd9258b0c8c43e3dce8566e0cf53c256067e6e
Kaeneth Dayao <kendayao@gmail.com>
2020-10-05 11:26:32 -0700

    create and styled note component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import Note from './components/note/Note'
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <Note />
       <Footer />
     </div>
   )
