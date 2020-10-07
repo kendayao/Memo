a4cbe1a2514fc0e297d306f50cd43c1a5cb4ef98
Kaeneth Dayao <kendayao@gmail.com>
2020-10-07 14:22:12 -0700

    created onChange function and note state

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Form.css'
 
 function Form(){
 
+    const[note, setNote]=useState({
+        title:"",
+        content:""
+    })
+
+    function handleInputChange(event){
+        event.preventDefault()
+        const {name, value}=event.target
+
+        setNote(prevNote=>{
+            return {...prevNote, [name]:value}
+        })
+    }
+    console.log(note)
+
     return(
         <div>
             <form>
-                <input name='title' placeholder='Title'></input>
-                <textarea name='content' placeholder='Take a note...' rows="3"></textarea>
+                <input onChange={handleInputChange} value={note.title} name='title' placeholder='Title'></input>
+                <textarea onChange={handleInputChange} name='content' placeholder='Take a note...' rows="3" value={note.content}></textarea>
                 <button>Add</button>
             </form>
         </div>
