03016c56954e50d5dd37340f1627daa21f449936
Kaeneth Dayao <kendayao@gmail.com>
2020-10-08 13:10:04 -0700

    logo edit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 function Header(){
     return (
         <header>
-            <img className="logo" alt="memo-logo" src="../../../public/memo_logo.png" /><h1>Memo</h1>
+            <h1>Memo</h1>
         </header>
     )
 }
