31953cb6ed482ce3c711eb755eaf52b3c4f0b879
Kaeneth Dayao <kendayao@gmail.com>
2020-10-08 12:36:52 -0700

    Updates

---
+++ precache-manifest.702a5882df7a61141b6ce972ca4a3f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14905ddd02746dc35c5094c37886ce3",
+    "url": "/Memo/index.html"
+  },
+  {
+    "revision": "c37267d048e770a5bea0",
+    "url": "/Memo/static/css/main.a2db0f3e.chunk.css"
+  },
+  {
+    "revision": "b3165fbcd7ff71fd1d0c",
+    "url": "/Memo/static/js/2.1980dd2b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Memo/static/js/2.1980dd2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37267d048e770a5bea0",
+    "url": "/Memo/static/js/main.7f6a9fe8.chunk.js"
+  },
+  {
+    "revision": "51a855d7f224533056d3",
+    "url": "/Memo/static/js/runtime-main.61169e87.js"
+  }
+]);
